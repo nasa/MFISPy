f53f1d3544b5ffe41a9d7412e144c4520f64daa7
Cole <dacole2@ndc.nasa.gov>
2020-06-09 09:54:58 -0400

    initial upload of tests (to be written)

---
+++ tests/test_biasing_dist.py
@@ -0,0 +1,27 @@
+import pytest
+import numpy as np
+from sklearn.mixture import GaussianMixture
+from scipy.stats import multivariate_normal
+from mfis import BiasingDist
+
+
+def test_train(self, Z, max_clusters):
+    
+
+def test_lowest_bic_gmm(Z, max_clusters):
+    
+
+def test_density(self, Z):
+
+
+def test_density_in_cluster(self, Z, cluster):
+
+
+def test_build_covar_matrix(self, cluster):
+            
+  
+def test_build_diag_covar_matrix(self, cluster):
+
+        
+
+    
---
+++ tests/test_multifidelity_IS.py
@@ -0,0 +1,9 @@
+import pytest
+import numpy as np
+from mfis import multiIS
+      
+def test_calc_importance_weights(z_p, p_dist, q_dist):
+
+
+def test_mfip_estimate(self, z_p, high_fidelity_Y, p_dist, q_dist):
+
