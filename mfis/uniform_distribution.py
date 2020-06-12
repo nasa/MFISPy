90e3beb1cb4fcda68df0d35e886657605cf7280c
Cole <dacole2@ndc.nasa.gov>
2020-06-12 16:09:20 -0400

    added zero probability to samples outside bounds

--- mfis/uniform_distribution.py
+++ mfis/uniform_distribution.py
@@ -18,17 +18,28 @@ class IndependentUniformDistribution(InputDistribution):
         return samples
 
     def evaluate_pdf(self, samples):
-        self._samples_within_bounds(samples)
-        
         p = np.ones((samples.shape[0],1))
     
         for i,bound in enumerate(self._bounds):
             p *= 1/(bound[1] - bound[0])
-
+        indices_outside_bounds_samples = \
+                self.indices_of_samples_outside_bounds(samples)
+        p[indices_outside_bounds_samples] = 0
         return p
     
-    def _samples_within_bounds(self, samples):
+    def check_all_samples_within_bounds(self, samples):
         for i,bound in enumerate(self._bounds):
             if not (np.min(samples[:,i]) >= bound[0] and 
             np.max(samples[:,i]) <= bound[1]):
                 raise ValueError("Not all samples within bounds")
+                
+    def indices_of_samples_outside_bounds(self, samples):
+        outside_sample_indices = []
+        for i,bound in enumerate(self._bounds):
+            outside_indicators = ((samples[:,i] <= bound[0]) | 
+                                  (samples[:,i] >= bound[1]))
+            indices = [i for i, x in enumerate(outside_indicators) if x]
+            outside_sample_indices = outside_sample_indices + indices   
+        
+        return np.unique(outside_sample_indices)             
+                
