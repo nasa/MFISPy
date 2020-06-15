ad8e6e1f22680ad82b27299674b819fd3a6e5cb8
Cole <dacole2@ndc.nasa.gov>
2020-06-15 08:45:56 -0400

    added tests for samples outside of bounds

--- mfis/uniform_distribution.py
+++ mfis/uniform_distribution.py
@@ -1,12 +1,13 @@
 import numpy as np
 from .input_distribution import InputDistribution
 
-class IndependentUniformDistribution(InputDistribution):
+class IndependentUniformDistribution():
 
     def __init__(self, bounds, seed=None):
         self._bounds = bounds
         if seed is not None:
             np.random.seed(seed)
+
 
     def draw_samples(self, num_samples):
 
@@ -17,29 +18,42 @@ class IndependentUniformDistribution(InputDistribution):
 
         return samples
 
+
     def evaluate_pdf(self, samples):
-        p = np.ones((samples.shape[0],1))
-    
-        for i,bound in enumerate(self._bounds):
-            p *= 1/(bound[1] - bound[0])
+        p = self._get_uniform_density(samples.shape[0])
+        
         indices_outside_bounds_samples = \
-                self.indices_of_samples_outside_bounds(samples)
-        p[indices_outside_bounds_samples] = 0
+                self._indices_of_samples_outside_bounds(samples)
+        if len(indices_outside_bounds_samples) > 0:
+            p[indices_outside_bounds_samples] = 0
+        
         return p
+    
     
     def check_all_samples_within_bounds(self, samples):
         for i,bound in enumerate(self._bounds):
             if not (np.min(samples[:,i]) >= bound[0] and 
             np.max(samples[:,i]) <= bound[1]):
                 raise ValueError("Not all samples within bounds")
-                
-    def indices_of_samples_outside_bounds(self, samples):
+     
+        
+    def _get_uniform_density(self, num_of_samples):
+        uniform_density = 1
+        for i,bound in enumerate(self._bounds):
+            uniform_density *= 1/(bound[1] - bound[0])
+        p = np.ones((num_of_samples,1)) * uniform_density
+        
+        return p  
+
+       
+    def _indices_of_samples_outside_bounds(self, samples):
         outside_sample_indices = []
         for i,bound in enumerate(self._bounds):
-            outside_indicators = ((samples[:,i] <= bound[0]) | 
-                                  (samples[:,i] >= bound[1]))
+            outside_indicators = ((samples[:,i] < bound[0]) | 
+                                  (samples[:,i] > bound[1]))
             indices = [i for i, x in enumerate(outside_indicators) if x]
             outside_sample_indices = outside_sample_indices + indices   
         
         return np.unique(outside_sample_indices)             
-                
+
+
